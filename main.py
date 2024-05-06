bd3e6892536ca081c5d9f08587a2a978fa894e4c
Zachary Keyes <56801478+imzoc@users.noreply.github.com>
2024-05-06 11:29:26 -0700

    Documenting main.py

--- main.py
+++ main.py
@@ -10,4 +10,12 @@ def hypothesis2(author):
     books = analyzer.books_by_author[author]
     analyzer.compare_distribution(books, kind='book')
 
+hypothesis1()
+
+# matplotlib plots stall the execution of the python script
+# and terminate the python script when you close the plot,
+# so I ran hypothesis 2 manually for each author (by putting
+# a different author first in line on each execution).
 hypothesis2('dickens')
+hypothesis2('austen')
+hypothesis2('tolstoy')
